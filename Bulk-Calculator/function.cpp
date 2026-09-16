0bad98c61191720665d91cba0775d637a22bb549
Assassin Spider <raif75219@gmail.com>
2026-09-16 17:38:03 +0500

    Refactor welcome and section functions for bulk calculator

--- Bulk-Calculator/function.cpp
+++ Bulk-Calculator/function.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 void welcome(){
+    cout << "=====================" << endl;
+    cout << "   Bulk Calculator   " << endl;
+    cout << "=====================" << endl;
+    cout << endl;
+    cout << "1. Multiplier" << endl;
+    cout << "2. Addition" << endl;
+    cout << "3. Exit" << endl;
+    cout << endl;
+    cout << "Input: " << endl;
+}
+
+void multiplicationsection(){
     cout << "=================" << endl;
     cout << "   Multiplier    " << endl;
     cout << "=================" << endl;
@@ -10,6 +22,18 @@ void welcome(){
     cout << "2. Multiply number with PI(one number multiply by PI)" << endl;
     cout << "3. Multiply numbers in bulk" << endl;
     cout << "4. Exit" << endl;
+    cout << endl;
+    cout << "Input: " << endl;
+}
+
+void additionsection(){
+    cout << "===============" << endl;
+    cout << "   ADDITION    " << endl;
+    cout << "===============" << endl;
+    cout << endl;
+    cout << "1. Addition(Multiply only 2 numbers at a time)" << endl;
+    cout << "2. Add numbers in bulk" << endl;
+    cout << "3. Exit" << endl;
     cout << endl;
     cout << "Input: " << endl;
 }
