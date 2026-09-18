aa55192b3dd1cf5dd8069e39de7232305777327c
Assassin Spider <raif75219@gmail.com>
2026-09-18 18:53:38 +0500

    Refactor main function for better input handling

--- Bulk-Calculator/main.cpp
+++ Bulk-Calculator/main.cpp
@@ -143,6 +143,9 @@ int main(){
                             goback();
                         }
                         else if(exponentiationuserinput == 3){}
+                        else if(exponentiationuserinput == 4){
+                            break;
+                        }
                     }
                 }
             }
